e1ab84e397797aa1e3f2ae89aa306e6dd5d6602e
OriginatorX <dreamflow@mail.ru>
2022-10-09 20:11:43 +1000

    project update

--- statics/app.js
+++ statics/app.js
@@ -2,14 +2,14 @@ function handleInput() {
     const inputfield = document.getElementById('inputfield');
     
     const pretendNumber = Number(inputfield.value);
-    if (Number.isNaN(pretendNumber) || pretendNumber === 0) {
-        console.log('The entered data is not a number');
+    if (Number.isNaN(pretendNumber) || pretendNumber <= 0) {
+        formatOutput(pretendNumber, 'an invalid')
         return;
     }
 
     isPrime(pretendNumber) 
-        ? formatOutput(pretendNumber, 'prime')
-        : formatOutput(pretendNumber, 'composite');
+        ? formatOutput(pretendNumber, 'a prime')
+        : formatOutput(pretendNumber, 'a composite');
 }
 
 function formatOutput(number, kind) {
@@ -17,14 +17,15 @@ function formatOutput(number, kind) {
     const outputNumber = document.getElementById('outputNumber');
 
     outputNumber.textContent = String(number);
-    outputfield.textContent = `is a ${kind} number`;
+    outputfield.textContent = `is ${kind} number`;
 }
 
 function isPrime(pretendNumber) {
     for (let i = 2; Math.pow(i, 2) <= pretendNumber; i++) {
         if (pretendNumber % i == 0) {
             return false;
-        } 
+        }
+        console.log('item'); 
     }
     return pretendNumber === 1 ? false : true;
 }
